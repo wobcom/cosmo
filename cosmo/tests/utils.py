80bf477f5a41e1212a0ac40c330c5c15c6a0b281
lou lecrivain <lou.lecrivain@wdz.de>
2024-06-04 13:22:34 +0200

    add common setup helper

--- cosmo/tests/utils.py
+++ cosmo/tests/utils.py
@@ -1,5 +1,25 @@
 import json
 
+class CommonSetup:
+    PROGNAME='cosmo'
+    TEST_URL = 'https://netbox.example.com'
+    TEST_TOKEN = 'token123'
+    DEFAULT_ENVIRON = {'NETBOX_URL': TEST_URL, 'NETBOX_API_TOKEN': TEST_TOKEN}
+    DEFAULT_CFGFILE = 'cosmo.example.yml'
+
+    def __init__(
+            self, mocker, environ=DEFAULT_ENVIRON,
+            cfgFile=DEFAULT_CFGFILE, args=[PROGNAME]):
+        self.mocker = mocker
+        # we need to setup correct test environ before running
+        self.environPatch = self.mocker.patch.dict('os.environ', environ)
+        # patch args, since current ones are from pytest call
+        self.argsPatch = self.mocker.patch('sys.argv', args)
+        # patch configuration file lookup if requested
+        self.cfgFilePatch = None
+        if cfgFile:
+            self.cfgFilePatch = PatchIoFilePath(self.mocker, 'cosmo.__main__', 'cosmo.yml', cfgFile)
+
 class RequestResponseMock:
     def __init__(self, **kwargs):
         self.status_code = kwargs['status_code']
@@ -35,5 +55,3 @@ class PatchIoFilePath:
         self.mocker.patch(self.namespace + '.os.path.isfile',
                           lambda f: True if f == self.requestedPath else False)
 
-def patchEnviron(mocker, environ):
-    mocker.patch.dict('os.environ', environ)
