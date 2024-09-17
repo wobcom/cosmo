65c2641338afbb50f5e2d727dcee089fba1159cd
lou lecrivain <lou.lecrivain@wdz.de>
2024-09-17 14:03:57 +0200

    add typename for reliable object deserialization

--- cosmo/graphqlclient.py
+++ cosmo/graphqlclient.py
@@ -25,15 +25,13 @@ class GraphqlClient:
         return r.json()
 
     def get_data(self, device_config):
-        # TODO add typename everywhere so that the custom JSON parser
-        # can have JSONRPC2.0-style interface and no hazardous
-        # field-guessing for object types
         query_template = Template(
             """
             {
               device_list(
                 name: $device_array,
               ) {
+                __typename
                 id
                 name
                 serial
@@ -42,15 +40,19 @@ class GraphqlClient:
                   slug
                 }
                 platform {
+                  __typename
                   manufacturer {
+                   __typename
                     slug
                   }
                   slug
                 }
                 primary_ip4 {
+                  __typename
                   address
                 }
                 interfaces {
+                  __typename
                   id
                   name
                   enabled
@@ -60,40 +62,53 @@ class GraphqlClient:
                   mac_address
                   description
                   vrf {
+                    __typename
                     id
                   }
                   lag {
+                    __typename
                     id
                   }
                   ip_addresses {
+                    __typename
                     address
                   }
                   untagged_vlan {
+                    __typename
                     id
                     name
                     vid
                   }
                   tagged_vlans {
+                    __typename
                     id
                     name
                     vid
                   }
                   tags {
+                    __typename
                     name
                     slug
                   }
                   parent {
+                    __typename
                     id
                   }
                   connected_endpoints {
+                    __typename
                     ... on InterfaceType {
+                      __typename
                       name
                       device {
+                        __typename
                         primary_ip4 {
+                          __typename
                           address
                         }
                         interfaces {
+                          __typename
                           ip_addresses {
+                            __typename
                             address
                           }
                         }
@@ -103,37 +118,47 @@ class GraphqlClient:
                   custom_fields
                 }
                 staticroute_set {
+                  __typename
                   interface {
+                    __typename
                     name
                   }
                   vrf {
+                    __typename
                     name
                   }
                   prefix {
+                    __typename
                     prefix
                     family {
+                      __typename
                       value
                     }
                  }
                  next_hop {
+                    __typename
                    address
                  }
                  metric
                 }
               }
               vrf_list {
+                __typename
                 id
                 name
                 description
                 rd
                 export_targets {
+                  __typename
                   name
                 }
                 import_targets {
+                  __typename
                   name
                 }
               }
               l2vpn_list {
+                __typename
                 id
                 name
                 type
@@ -146,18 +171,24 @@ class GraphqlClient:
                       id
                     }
                     ... on InterfaceType {
+                      __typename
                       id
                       device {
+                        __typename
                         name
                         interfaces (type: "virtual") {
+                          __typename
                           ip_addresses {
+                            __typename
                             address
                           }
                           parent {
+                            __typename
                             name
                             type
                           }
                           vrf {
+                            __typename
                             id
                           }
                         }
