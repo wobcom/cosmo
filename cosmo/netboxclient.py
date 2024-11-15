55530de2927656733ddf75d7b560033e29e5b1a4
Johann Wagner <johann.wagner@wobcom.de>
2024-11-15 20:15:11 +0100

    Splitted, multi-threaded and rewamped some queries to speed up performance

--- cosmo/netboxclient.py
+++ cosmo/netboxclient.py
@@ -1,4 +1,6 @@
 import json
+import time
+from multiprocessing.pool import Pool
 from string import Template
 from urllib.parse import urlencode
 
@@ -34,7 +36,13 @@ class NetboxClient:
         return json['netbox-version']
 
     def get_data(self, device_config):
-        return self.child_client.get_data(device_config)
+        start_time = time.perf_counter()
+        data = self.child_client.get_data(device_config)
+        end_time = time.perf_counter()
+        diff_time = end_time - start_time
+        print(f"[INFO] Data fetching took {round(diff_time, 2)} s...")
+
+        return data
 
 
 class NetboxStrategy:
@@ -87,6 +95,43 @@ class NetboxStrategy:
 class NetboxV4Strategy(NetboxStrategy):
 
     def get_data(self, device_config):
+        device_list = device_config['router'] + device_config['switch']
+
+        with Pool() as pool:
+            device_data = pool.apply_async(self.get_base_data, (device_list,))
+            l2vpn_data = pool.apply_async(self.get_l2vpn_data, ())
+            connected_devices_data = pool.apply_async(self.get_connected_devices_data, ())
+            static_route_data = pool.apply_async(self.get_static_route_data, (device_list,))
+
+            device_data = device_data.get()
+            l2vpn_data = l2vpn_data.get()
+            connected_devices_data = connected_devices_data.get()
+            static_route_data = static_route_data.get()
+
+        graphql_data = {
+            **device_data,
+            **l2vpn_data,
+        }
+
+        for d in graphql_data['device_list']:
+            device_static_routes = list(filter(lambda sr: str(sr['device']['id']) == d['id'], static_route_data))
+            d['staticroute_set'] = device_static_routes
+
+            for interface in d['interfaces']:
+                cd_interface = next(filter(lambda i: i["id"] == interface["id"], connected_devices_data['interface_list']), None)
+                if not cd_interface:
+                    continue
+
+                parent_interface = next(filter(lambda i: i['id'] == cd_interface['parent']['id'], d['interfaces']), None)
+                parent_interface['connected_endpoints'] = cd_interface['parent']['connected_endpoints']
+
+        return graphql_data
+
+    def get_static_route_data(self, device_list):
+        static_routes = self.query_rest("api/plugins/routing/staticroutes/", {"device": device_list})
+        return static_routes
+
+    def get_base_data(self, device_list):
         query_template = Template(
             """
             query {
@@ -109,7 +154,7 @@ class NetboxV4Strategy(NetboxStrategy):
                 primary_ip4 {
                   address
                 }
-                
+
                 interfaces {
                   id
                   name
@@ -145,6 +190,82 @@ class NetboxV4Strategy(NetboxStrategy):
                   parent {
                     id
                   }
+                  custom_fields
+                }
+              }
+              vrf_list {
+                id
+                name
+                description
+                rd
+                export_targets {
+                  name
+                }
+                import_targets {
+                  name
+                }
+              }
+
+            }"""
+        )
+
+        query = query_template.substitute(
+            device_array=json.dumps(device_list)
+        )
+        return self.query(query)['data']
+
+    def get_l2vpn_data(self):
+        l2vpn_template = Template(
+            """
+            query {
+                l2vpn_list (filters: {name: {starts_with: "WAN: "}}) {
+                    id
+                    name
+                    type
+                    identifier
+                    terminations {
+                      id
+                      assigned_object {
+                        __typename
+                        ... on VLANType {
+                          id
+                        }
+                        ... on InterfaceType {
+                          id
+                          device {
+                            name
+                            interfaces (filters:{type: {exact: "virtual"}}) {
+                              ip_addresses {
+                                address
+                              }
+                              parent {
+                                name
+                                type
+                              }
+                              vrf {
+                                id
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                }
+              }
+              """
+
+        )
+
+        return self.query(l2vpn_template.substitute())['data']
+
+    def get_connected_devices_data(self):
+        connected_devices_template = Template(
+            """
+            query {
+              interface_list(filters: { tag: "bgp_cpe" }) {
+                id,
+                parent {
+                  id,
                   connected_endpoints {
                     ... on InterfaceType {
                       name
@@ -160,70 +281,10 @@ class NetboxV4Strategy(NetboxStrategy):
                       }
                     }
                   }
-                  custom_fields
-                }
-                
-              }
-              vrf_list {
-                id
-                name
-                description
-                rd
-                export_targets {
-                  name
-                }
-                import_targets {
-                  name
                 }
               }
-              l2vpn_list (filters: {name: {starts_with: "WAN: "}}) {
-                id
-                name
-                type
-                identifier
-                terminations {
-                  id
-                  assigned_object {
-                    __typename
-                    ... on VLANType {
-                      id
-                    }
-                    ... on InterfaceType {
-                      id
-                      device {
-                        name
-                        interfaces (filters:{type: {exact: "virtual"}}) {
-                          ip_addresses {
-                            address
-                          }
-                          parent {
-                            name
-                            type
-                          }
-                          vrf {
-                            id
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }"""
+            }
+            """
         )
 
-        device_list = device_config['router'] + device_config['switch']
-        query = query_template.substitute(
-            device_array=json.dumps(device_list)
-        )
-
-        r = self.query(query)
-        graphql_data = r['data']
-
-        static_routes = self.query_rest("api/plugins/routing/staticroutes/", {"device": device_list})
-
-        for d in r['data']['device_list']:
-            device_static_routes = list(filter(lambda sr: str(sr['device']['id']) == d['id'], static_routes))
-            d['staticroute_set'] = device_static_routes
-
-        return graphql_data
+        return self.query(connected_devices_template.substitute())['data']
