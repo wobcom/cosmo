d28738dbef9f0489058c3a1a21128c6113d81ccc
Johann Wagner <johann.wagner@wobcom.de>
2025-03-10 18:29:49 +0100

    feat: Initial implementation of ip pools

--- cosmo/abstractroutervisitor.py
+++ cosmo/abstractroutervisitor.py
@@ -9,4 +9,5 @@ class AbstractRouterExporterVisitor(AbstractNoopNetboxTypesVisitor, ABC):
     _interfaces_key = "interfaces"
     _mgmt_vrf_name = "MGMT-ROUTING-INSTANCE"
     _l2circuits_key = "l2circuits"
+    _pools_key = "pools"
     _allowed_core_mtus = [9216, 9600, 9230, 9586, 9116]
