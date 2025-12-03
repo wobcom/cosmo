36076f7ac898ccb2faa89dfb2349bba785de1628
Yureka <yuka@yuka.dev>
2025-12-03 11:04:57 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/ef3a956f697525883b77192cbe208233ea0f8f79' (2025-04-09)
      → 'github:NixOS/nixpkgs/8bb5646e0bed5dbd3ab08c7a7cc15b75ab4e1d0f' (2025-11-30)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "cosmo - ???";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
   outputs = { self, nixpkgs, flake-utils }:
     {
