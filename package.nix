6faa526d64bcb231aa54d46f24fd639ece0ad27c
Yureka <yuka@yuka.dev>
2025-12-17 08:38:57 +0100

    flake: disable netbox tests on darwin

--- package.nix
+++ package.nix
@@ -1,4 +1,6 @@
-{ buildPythonApplication,
+{ lib,
+  stdenv,
+  buildPythonApplication,
   python,
   poetry-core,
   requests,
@@ -37,6 +39,16 @@ buildPythonApplication rec {
     pytestCheckHook
     pytest-mock
     coverage
+  ];
+
+  disabledTests = lib.optionals stdenv.hostPlatform.isDarwin [
+    # https://github.com/wobcom/cosmo/issues/85
+    "test_limit_argument_with_commas"
+    "test_limit_arguments_with_repeat"
+    "test_device_generation_ansible"
+    "test_device_generation_nix"
+    "test_device_processing_error"
+    "test_case_get_data"
   ];
 
   passthru = {
