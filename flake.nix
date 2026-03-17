9d6dce63584241884391cf121df5aaa7bcc25565
Ember 'n0emis' Keske <git@n0emis.eu>
2026-03-17 11:54:25 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/8bb5646e0bed5dbd3ab08c7a7cc15b75ab4e1d0f' (2025-11-30)
      → 'github:NixOS/nixpkgs/5b2c2d84341b2afb5647081c1386a80d7a8d8605' (2026-03-16)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "cosmo - ???";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, flake-utils }:
     {
